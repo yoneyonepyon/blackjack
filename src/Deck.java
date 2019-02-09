c498a2bbedd014c54e8786bb975ea204c48565a6
ぴょんぴょん <46988423+yoneyonepyon@users.noreply.github.com>
2019-02-09 15:40:54 +0900

    カードをひく挙動の追加

--- src/Deck.java
+++ src/Deck.java
@@ -3,6 +3,7 @@ import java.util.Random;
 public class Deck {
 
 	Card[] cards = new Card[52]; // 山札のカード
+	Card[] hand = new Card[0]; // 手札のカード
 	String name; // プレイヤーの名前
 
 	public Deck(String n) {
@@ -18,6 +19,45 @@ public class Deck {
 				cards[ii++] = c;
 			}
 		}
+	}
+
+	// 1枚カードをひく
+	public Card draw() {
+		Card[] c = draw(1);
+		return c[0];
+	}
+
+	// n枚分のカードを配列で返す
+	public Card[] draw(int n) {
+		// カードをこれ以上引けない場合はnullを返す
+		if (cards.length - n < 0) {
+			return null;
+		}
+
+		// 引いた手札、引いた分を加えた手札と残りのカード用の配列を用意
+		Card[] drawCards = new Card[n];
+		Card[] handCards = new Card[hand.length + n];
+		Card[] deckCards = new Card[cards.length - n];
+
+		// 現在の手札を新しい配列にコピー
+		for (int i = 0; i < hand.length; i++) {
+			handCards[i] = hand[i];
+		}
+
+		// 引いたカードを山札に加える
+		for (int i = 0; i < cards.length; i++) {
+			Card c = cards[i];
+			if (i < n) {
+				drawCards[i] = c;
+				handCards[i + hand.length] = c;
+			} else {
+				deckCards[i - n] = c;
+			}
+		}
+
+		cards = deckCards;
+		hand = handCards;
+		return drawCards;
 	}
 
 	// 山札をシャッフルする(おそらくすごい単純な方法)
@@ -41,12 +81,22 @@ public class Deck {
 
 	// カードのshowName()を実行する
 	public void showName() {
-		int total = 0;
+		System.out.println(name + "の山札:");
 		for (Card c : cards) {
 			c.showName();
-			total++;
 		}
-		System.out.println(total);
+		System.out.println("山札の枚数: " + cards.length);
+	}
+
+	// 手札のshowName()を実行する
+	public void showHand() {
+		System.out.println(name + "の手札:");
+		for (Card c : hand) {
+			if (c != null) {
+				c.showName();
+			}
+		}
+		System.out.println("手札の枚数: " + hand.length);
 	}
 
 }
--- src/Game.java
+++ src/Game.java
@@ -3,7 +3,11 @@ public class Game {
 
 	public static void main(String[] args) {
 		Deck d = new Deck("プレイヤー");
-		d.shuffle(3); // 3回ほど混ぜる
+		// d.shuffle(3); // 3回ほど混ぜる
+		d.draw(2); // 二枚ドローする
+		d.showHand(); // 手札を見せる
+		d.draw(); // 二枚ドローする
+		d.showHand(); // 手札を見せる
 		d.showName();
 	}
 
